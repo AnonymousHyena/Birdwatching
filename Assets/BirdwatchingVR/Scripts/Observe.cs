40383d63cd8680f2e1738a843d4b0e26e9c2fa3b
AnonymousHyena <stathisbozikas@gmail.com>
2020-05-15 16:12:19 +0300

    Initital Commit; Island scene complete; bird shooting basics implemented

---
+++ Assets/BirdwatchingVR/Scripts/Observe.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observe : MonoBehaviour {
+    Camera currentCamera;
+    Ray ray;
+    RaycastHit[] hits;
+
+
+    void Start()
+    {
+        currentCamera = Camera.main;
+    }
+
+    public void Scare()
+    {
+        ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        hits = Physics.RaycastAll(currentCamera.transform.position, currentCamera.transform.forward);//(ray,60);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.tag == "lb_bird")
+            {
+                hit.transform.SendMessage("Flee");
+              //  break;
+            }
+        }
+    }
+
+    public void See()
+    {
+        ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        hits = Physics.RaycastAll(currentCamera.transform.position, currentCamera.transform.forward);//(ray,60);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.tag == "lb_bird")
+            {
+                hit.transform.SendMessage("KillBirdWithForce", ray.direction * 500);
+              //  break;
+            }
+        }
+    }
+}
---
+++ Assets/BirdwatchingVR/Scripts/SunMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour {
+
+    public GameObject directionalLight;
+	
+	// Update is called once per frame
+	void Update () {
+        directionalLight.transform.Rotate(0, Time.deltaTime, 0);
+	}
+}
